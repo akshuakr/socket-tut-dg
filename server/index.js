00d7602be58aebb1ce46838848df64966ee02053
akshu <in.akr.2000@gmail.com>
2023-10-29 12:26:22 +0530

    chapter 3 completed

--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
-import { createServer } from "http";
+import express from "express";
 import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const httpServer = createServer();
+const misc = import.meta.url;
+console.log(misc);
 
-const io = new Server(httpServer, {
+const __filename = fileURLToPath(misc);
+console.log(__filename);
+
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+const io = new Server(expressServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500","http://127.0.0.1:5500"],
+        origin:
+            process.env.NODE_ENV === "production"
+                ? false
+                : ["http://localhost:5500", "http://127.0.0.1:5500"],
     },
 });
 
 io.on("connection", (socket) => {
-    
     console.log(`User ${socket.id} connected`);
 
     socket.on("message", (data) => {
@@ -18,7 +37,3 @@ io.on("connection", (socket) => {
         io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
     });
 });
-
-
-
-httpServer.listen(3500, () => console.log(`listening on port 3500`));
